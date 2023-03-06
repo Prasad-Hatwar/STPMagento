45bf24016c49bcf52344c0aeaa14e170354e2d20
drpra <drpra@LAPTOP-CQ5F5C01>
2023-03-06 16:11:42 +0530

    new changes on account page

--- src/main/java/magento/pages/CreateAccountPage.java
+++ src/main/java/magento/pages/CreateAccountPage.java
@@ -2,5 +2,11 @@ package magento.pages;
 
 public class CreateAccountPage {
 	
+	private void xyz() {
+		
+		System.out.println("this is a crate account page from magento framework ");
+		System.out.println();
+
+	}
 
 }
